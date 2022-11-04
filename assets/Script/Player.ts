28d3fa00b834ea604faa67cdce16ce71e7259df8
108062116 <inorixac@gmail.com>
2022-11-04 23:56:02 +0800

    init commit

---
+++ assets/Script/Player.ts
@@ -0,0 +1,103 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class Player extends cc.Component {
+
+    @property(cc.Node)
+    label: cc.Label = null;
+
+    private onGround : Boolean = true;
+    private movedir : number = 0; // right for 1 and left for -1 , idle is 0
+    private scaledir : number = 1;
+    private playerSpeed : number = 200;
+    private aDown : Boolean = false;
+    private dDown : Boolean = false;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        cc.director.getPhysicsManager().enabled = true;
+        
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    }
+
+    // onDestroy () {
+    //     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    //     cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+    // }
+
+    start () {
+        cc.director.getCollisionManager().enabled = true;
+    }
+
+    update(dt){
+        this.node.x += this.playerSpeed * this.movedir * dt;
+        this.node.scaleX = this.scaledir;
+        // cc.log(this.onGround);
+    }
+
+    onKeyDown(event){
+        switch (event.keyCode){
+            case cc.macro.KEY.a:
+                this.playerMove(-1);
+                break;
+            case cc.macro.KEY.d:
+                this.playerMove(1);
+                break;
+            case cc.macro.KEY.space:
+                this.playerjump();
+                break;
+        }
+    }
+
+    onKeyUp(event)
+    {
+        switch(event.keyCode)
+        {
+            case cc.macro.KEY.a:
+                this.aDown = false;
+                if(this.aDown)
+                    this.playerMove(-1);
+                else
+                    this.playerMove(0);
+                break;
+            case cc.macro.KEY.d:
+                this.dDown = false;
+                if(this.dDown)
+                    this.playerMove(1);
+                else
+                    this.playerMove(0);
+                break;
+        }
+    }
+
+
+    playerMove(moveDir: number) {
+        this.movedir = moveDir;
+        if(moveDir != 0) this.scaledir = moveDir;
+    }
+
+    playerjump() {
+        cc.log('onGround : ' + this.onGround);
+        if (this.onGround) {
+            this.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 1000);
+            this.onGround = false;
+        }
+    }
+
+    onBeginContact(contact , self , other){
+        cc.log(other.node.name);
+        if(other.node.name == "Background"){
+            cc.log('onGround : ' + this.onGround);
+            this.onGround = true;
+        }
+    }
+}
