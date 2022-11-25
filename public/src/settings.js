3501c19453501718e5603e177a9d16d9d9033a04
QQAI13 <qqai@gapp.nthu.edu.tw>
2022-11-25 17:53:40 +0800

    update rule

---
+++ public/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Menu.fire",orientation:"",jsList:["assets/Script/firebase.js"]};
